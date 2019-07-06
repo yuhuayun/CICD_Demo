a44a19c95439af68aaf5f72d36c80bda56c638d0
yuhuayun <example@163.com>
2019-07-06 18:44:03 +0800

    test

--- docker/run.sh
+++ docker/run.sh
@@ -3,7 +3,7 @@
 cp ${WORKSPACE}/target/cicd-demo-1.0.jar ${WORKSPACE}/docker/app.jar
 cd ${WORKSPACE}/docker
 docker build -t 172.16.0.193:5000/cloud/cicd:v${BUILD_NUMBER} .
-#docker push 172.16.0.193:5000/cloud/cicd:v${BUILD_NUMBER}
+docker push 172.16.0.193:5000/cloud/cicd:v${BUILD_NUMBER}
 
 sed -i 's/\$\$BUILD_NUMBER\$\$/'${BUILD_NUMBER}'/g' tomcat-deployment.yaml
 
