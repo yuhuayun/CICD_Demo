af793c58f38763fd4d17133faad7dad6e8d6d0f4
yuhuayun <example@163.com>
2019-07-06 16:49:21 +0800

    dockerfile

--- docker/run.sh
+++ docker/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build 172.16.0.193:5000/cloud/cicd:latest .
+docker build -t 172.16.0.193:5000/cloud/cicd:latest .
 docker run -d --restart=always -p 8080:8080 --name cicd 172.16.0.193:5000/cloud/cicd:latest
