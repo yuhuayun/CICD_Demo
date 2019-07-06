390a34c9c7e8de1bb109a947affa6dc26365a69e
yuhuayun <example@163.com>
2019-07-06 17:50:40 +0800

    k8s

--- docker/run.sh
+++ docker/run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-cd ${WORKSPACE}/docker
 
 cp ${WORKSPACE}/target/cicd-demo-1.0.jar ${WORKSPACE}/docker/app.jar
-
+cd ${WORKSPACE}/docker
 docker build -t 172.16.0.193:5000/cloud/cicd:v${BUILD_NUMBER} .
+#docker push 172.16.0.193:5000/cloud/cicd:v${BUILD_NUMBER}
 
-docker push 172.16.0.193:5000/cloud/cicd:v${BUILD_NUMBER}
+sed -i 's/\$\$BUILD_NUMBER\$\$/'${BUILD_NUMBER}'/g' tomcat-deployment.yaml
 
-docker run -d --restart=always -p 8080:8080 --name cicd 172.16.0.193:5000/cloud/cicd:v${BUILD_NUMBER}
+
+#docker run -d --restart=always -p 8080:8080 --name cicd 172.16.0.193:5000/cloud/cicd:v${BUILD_NUMBER}
