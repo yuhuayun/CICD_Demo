51e18c74a137a54e43674da7ee4715fd837f0102
yuhuayun <example@163.com>
2019-07-12 16:13:26 +0800

    testfile

--- docker/run.sh
+++ docker/run.sh
@@ -5,7 +5,7 @@ cd ${WORKSPACE}/docker
 docker build -t 172.16.0.193:5000/cloud/cicd:v${BUILD_NUMBER} .
 docker push 172.16.0.193:5000/cloud/cicd:v${BUILD_NUMBER}
 
-sed -i 's/\$\$BUILD_NUMBER\$\$/'${BUILD_NUMBER}'/g' tomcat-deployment.yaml
+sed -i 's/\$\$BUILD_NUMBER\$\$/'${BUILD_NUMBER}'/g' tomcat-test.yaml
 
 
 #docker run -d --restart=always -p 8080:8080 --name cicd 172.16.0.193:5000/cloud/cicd:v${BUILD_NUMBER}
